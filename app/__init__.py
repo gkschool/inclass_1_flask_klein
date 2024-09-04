4e504991684849e343b5b4b119e772a0d312ffb5
gkschool <george.klein@bobcats.gcsu.edu>
2024-09-04 13:52:07 -0400

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, g
-from app_factory import create_app
+from .app_factory import create_app
 
 app = create_app()
 
+from . import routes
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,6 @@
 from flask import render_template
+from . import app
 
+@app.route('/')
+def index():
+    return render_template('index.html')
